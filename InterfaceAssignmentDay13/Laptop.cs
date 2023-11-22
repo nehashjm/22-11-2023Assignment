d3c459f71bfe3a3e3088b738299910cd4e0f5fb7
Nehash <nehash.j.m027@gmail.com>
2023-11-22 15:30:28 +0530

    initial commit

---
+++ InterfaceAssignmentDay13/Laptop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssignmentDay13
+{
+    public class Laptop : IConnectable, IRechargable, IDisplayable
+    {
+        void IConnectable.Dispay()
+        {
+            Console.WriteLine("I am an Iconnectable Laptop class");
+        }
+
+        void IRechargable.Display()
+        {
+            Console.WriteLine("I am an IRechargable Laptop class");
+        }
+
+        void IDisplayable.Display()
+        {
+            Console.WriteLine("I am an IDisplayable Laptop class");
+        }
+    }
+}
---
+++ InterfaceAssignmentDay13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssignmentDay13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop lap = new Laptop();
+            IConnectable connectable = lap;
+            connectable.Dispay();
+            IRechargable rechargable = lap;
+            rechargable.Display();
+            IDisplayable dispayable = lap;
+            dispayable.Display();
+            Smartphone smt = new Smartphone();
+            IConnectable connectable1 = smt;
+            connectable1.Dispay();
+            IDisplayable connectable2 = smt;
+            connectable2.Display();
+            IRechargable connectable3 = smt;
+            connectable3.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
